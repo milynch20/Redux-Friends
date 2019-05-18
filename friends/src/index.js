1d314a01492e70b117c83c6c8792dd5805f69d00
Malcolm Lynch <milynch1320@gmail.com>
2019-05-18 09:47:07 -0500

    updated files

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {friendReducer} from './Reducer/FriendsReducer';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+const store = createStore(
+
+    friendReducer,
+    applyMiddleware(thunk,logger)
+)
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+ReactDOM.render(
+    <Router>
+        <Provider store ={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </Router>   , document.getElementById('root'));
